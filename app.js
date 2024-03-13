3d2e237360a9e18ed30e1052e07301a5f5b98881
Rutul Lathiya <rutull009@gmail.com>
2024-03-14 00:15:50 +0530

    init3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express()
-const PORT=5500
+const PORT=5501
 const User=require("./models/user")
 app.set("view engine","ejs")
 app.use(express.urlencoded())
